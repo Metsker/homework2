7805dc4b41705fbf2456366c6e5a38c93517989e
Lev <66064249+Metsker@users.noreply.github.com>
2020-10-09 16:36:04 +0200

    Update Program.cs

--- Программирование линейных алгоритмов/Программирование линейных алгоритмов/Program.cs
+++ Программирование линейных алгоритмов/Программирование линейных алгоритмов/Program.cs
@@ -14,17 +14,17 @@ namespace RazvetAlg
             {
                 //Высокий уровень #28
                 Console.Write("Введите x1: ");
-                sbyte x1 = sbyte.Parse(Console.ReadLine());
+                int x1 = int.Parse(Console.ReadLine());
                 Console.Write("Введите y1: ");
-                sbyte y1 = sbyte.Parse(Console.ReadLine());
+                int y1 = int.Parse(Console.ReadLine());
                 Console.Write("Введите x2: ");
-                sbyte x2 = sbyte.Parse(Console.ReadLine());
+                int x2 = int.Parse(Console.ReadLine());
                 Console.Write("Введите y2: ");
-                sbyte y2 = sbyte.Parse(Console.ReadLine());
+                int y2 = int.Parse(Console.ReadLine());
                 Console.Write("Введите x3: ");
-                sbyte x3 = sbyte.Parse(Console.ReadLine());
+                int x3 = int.Parse(Console.ReadLine());
                 Console.Write("Введите y3: ");
-                sbyte y3 = sbyte.Parse(Console.ReadLine());
+                int y3 = int.Parse(Console.ReadLine());
                 double a = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
                 double b = Math.Sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
                 double c = Math.Sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
